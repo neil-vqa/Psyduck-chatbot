165bf89c07acef4f8f6f5dda5a29c35d9ae4fa8b
Neil Van <49968200+neil-vqa@users.noreply.github.com>
2019-07-13 22:09:27 +0800

    Add files via upload

---
+++ word_list.py
@@ -0,0 +1,84 @@
+
+def list1(text):
+    if text in ['Hi','Hello','hi','hello','Hey']:
+        return True
+    else:
+        pass
+
+def list2(text):
+    if text in ['Help','help','More keywords']:
+        return True
+    else:
+        pass
+
+def list3(text):
+    if text in ['Yummy','Suggest food','Recommend food','Food']:
+        return True
+    else:
+        pass
+
+def list4(text):
+    if text in ['Suggest movie','Recommend movie','What to watch?','Movie']:
+        return True
+    else:
+        pass
+
+def list5(text):
+    if text in ['Wat u think?','Tell me more','Speak','Tell me','wat u think?','Bored','Trivia']:
+        return True
+    else:
+        pass
+
+def list6(text):
+    if text in ['Life tip','life tip','Tip','tip']:
+        return True
+    else:
+        pass
+
+def list7(text):
+    if text in ['Tell me a quote','Quote','Give a quote','quote']:
+        return True
+    else:
+        pass
+
+def list8(text):
+    if text in ['World news','world news','news','News']:
+        return True
+    else:
+        pass
+
+def list9(text):
+    if text in ['Aww','Cute','Cutie','Awww','aww','cute']:
+        return True
+    else:
+        pass
+
+def list10(text):
+    if text in ['Send pic','Pic pls','Pic']:
+        return True
+    else:
+        pass
+
+def list11(text):
+    if text in ['TWICE','twice','Twice']:
+        return True
+    else:
+        pass
+
+def list12(text):
+    if text in ['Science', 'science', 'Tell me some science']:
+        return True
+    else:
+        pass
+
+def list13(text):
+    if text in ['Meme','meme','give meme','Give meme','Funny','funny']:
+        return True
+    else:
+        pass
+
+def list14(text):
+    if text in ['Shower','Random']:
+        return True
+    else:
+        pass
