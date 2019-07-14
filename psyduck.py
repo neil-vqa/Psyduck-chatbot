b7ec05df3b06d7ab4be276fea45c93f5e42dfaea
Neil Van <49968200+neil-vqa@users.noreply.github.com>
2019-07-14 20:03:27 +0800

    Update psyduck.py

--- psyduck.py
+++ psyduck.py
@@ -1,6 +1,6 @@
 
 from flask import Flask, request
-import json, praw
+import json, praw, template
 import requests, random, word_list
 
 app = Flask(__name__)
@@ -45,7 +45,7 @@ def post_this(gold, receiver, load):
             data=json.dumps({
                 "recipient": {"id": receiver},
                 "message": {"text": load,
-                           "quick_replies": quick_reps}
+                           "quick_replies": template.quick_reps()}
             }),
             headers={'Content-type': 'application/json'})
     
@@ -59,103 +59,9 @@ def post_pic(silver, sentto, picload):
                               "payload": {
                                 "url": picload
                               }},
-                           "quick_replies": quick_reps}
+                           "quick_replies": template.quick_reps()}
             }),
             headers={'Content-type': 'application/json'})
-
-def post_carousel(bronze, viewer, template):
-    r = requests.post("https://graph.facebook.com/v2.6/me/messages",
-            params={"access_token": bronze},
-            data=json.dumps({
-              "recipient": {"id": viewer},
-              "message":{
-                "attachment":{
-                  "type":"template",
-                  "payload":{
-                    "template_type":"generic",
-                    "elements":[
-                       {
-                        "title": template[0]['title'],
-                        "image_url": template[0]['poster'],
-                        "subtitle": template[0]['overview'],
-                        "buttons":[
-                              {
-                                "type":"web_url",
-                                "url": template[0]['movie_id'],
-                                "title":"Overview"
-                              }
-                        ]
-                       },
-                      {
-                        "title": template[1]['title'],
-                        "image_url": template[1]['poster'],
-                        "subtitle": template[1]['overview'],
-                        "buttons":[
-                              {
-                                "type":"web_url",
-                                "url": template[1]['movie_id'],
-                                "title":"Overview"
-                              }
-                        ]
-                      },
-                      {
-                        "title": template[2]['title'],
-                        "image_url": template[2]['poster'],
-                        "subtitle": template[2]['overview'],
-                        "buttons":[
-                              {
-                                "type":"web_url",
-                                "url": template[2]['movie_id'],
-                                "title":"Overview"
-                              }
-                        ]
-                      },
-                      {
-                        "title": template[3]['title'],
-                        "image_url": template[3]['poster'],
-                        "subtitle": template[3]['overview'],
-                        "buttons":[
-                              {
-                                "type":"web_url",
-                                "url": template[3]['movie_id'],
-                                "title":"Overview",
-                                "webview_height_ratio": "tall"
-                              }
-                        ]
-                      }
-                      ]
-                     }
-                    },
-                   "quick_replies": quick_reps}
-              }),
-              headers={'Content-type': 'application/json'})
-    
-quick_reps = [{
-    "content_type":"text",
-    "title":"Trivia",
-    "payload":"Trivia"
-},
-  {
-    "content_type":"text",
-    "title":"Quote",
-    "payload":"Quote"
-},
-  {
-    "content_type":"text",
-    "title":"News",
-    "payload":"News"
-},
-  {
-    "content_type":"text",
-    "title":"Cute",
-    "payload":"Cute"
-},
-  {
-    "content_type":"text",
-    "title":"More keywords",
-    "payload":"More keywords"
-}
-]
             
 def send_message(token, recipient, text):
     if word_list.list1(text.decode('unicode_escape')) == True:
@@ -202,7 +108,7 @@ def send_message(token, recipient, text):
             }
             movlis.append(movdat)
         payload = random.sample(movlis, 4)
-        post_carousel(token, recipient, payload)
+        template.movie_carousel(token, recipient, payload)
 
     elif word_list.list5(text.decode('unicode_escape')) == True:
         trivia = []
