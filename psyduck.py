e5fc5f1c993b3566000bfdc59386d8fbccf6d0f5
Neil Van <49968200+neil-vqa@users.noreply.github.com>
2019-07-26 09:14:19 +0800

    Update psyduck.py

--- psyduck.py
+++ psyduck.py
@@ -212,12 +212,12 @@ def send_message(token, recipient, text):
         payload1 = payloader['p_url']
         post_pic(token, recipient, payload1)
     
-    elif text.decode('unicode_escape') == "Photography service":
+    elif text.decode('unicode_escape') == ("Photography service" or "Photographer"):
         payload1 = "https://i.imgur.com/0LOlL2t.jpg"
         post_pic(token, recipient, payload1)
-        payload2 = "Ed Montes Photography: ........."
+        payload2 = "Ed Montes Photography: Captures your best moments at best angles. We provide creative portrait photography services."
         post_this(token, recipient, payload2)
-        payload3 = "See more of our quality portraits by visiting our page: https://www.facebook.com/edmontesphoto"
+        payload3 = "See more quality portraits by visiting the official page: https://www.facebook.com/edmontesphoto"
         post_this(token, recipient, payload3)
     
     elif ("Music" or "music") in text.decode('unicode_escape'):
